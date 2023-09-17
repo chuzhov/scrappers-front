d249e8c3adc13ba881ce3ea61d48a96e2330dd41
Dan Chuzhov <dan.chuzhov.htc@gmail.com>
2023-09-17 14:11:36 +0300

    add Header component

---
+++ src/layout/Header/Header.jsx
@@ -0,0 +1,25 @@
+import { LuMenu } from 'react-icons/lu';
+import { GiOctopus } from 'react-icons/gi';
+
+import './Header.css';
+import '../../variables.css';
+function Header({ user }) {
+  return (
+    <>
+      <div className="header">
+        <div className="user__wrapper">
+          <GiOctopus size={26} className="user__icon" />
+          <p>{user}</p>
+        </div>
+        <div class="menu__wrapper">
+          <button>
+            <LuMenu size={26} className="menu__icon" />
+          </button>
+        </div>
+      </div>
+      <h1 className="header__title">Збирач даних із сайтів Постачальників</h1>
+    </>
+  );
+}
+
+export default Header;
