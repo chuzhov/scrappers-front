f516a6a70f0b35e5b535434a60ac35220f2e70f9
Dan Chuzhov <dan.chuzhov.htc@gmail.com>
2023-09-22 13:49:17 +0300

    fix an issue with the state mutation

--- src/components/ScrappingDashboard/ScrappingDashboard.jsx
+++ src/components/ScrappingDashboard/ScrappingDashboard.jsx
@@ -1,10 +1,10 @@
 import { useEffect, useRef } from 'react';
 
 import ProgressButton from 'react-progress-button';
-import { BsDatabaseFillCheck } from 'react-icons/bs';
 
 import './ScrappingDashboard.css';
 import toShorterDate from 'utils/toShorterDate';
+import DashInfoLine from 'components/DashInfoLine/DashInfoLine';
 
 const ScrappingDashboard = ({
   user,
@@ -19,11 +19,20 @@ const ScrappingDashboard = ({
     if (socket) {
       socket.emit('generateReport', { email: user, target });
       setIsFetching(prevDashboardData => {
+        console.log('ScrappingDashboard');
+        console.dir(typeof prevDashboardData);
+        console.log(prevDashboardData[0]);
         return prevDashboardData.map(item => {
           if (item.target === target) {
-            item.isFetching = true;
+            // Return new object with updated isFetching
+            return {
+              ...item,
+              isFetching: true,
+            };
+          } else {
+            // Return unchanged item
+            return item;
           }
-          return item;
         });
       });
     }
@@ -37,36 +46,14 @@ const ScrappingDashboard = ({
     scrollToBottom();
   }, [data.data.progressMsg.length]);
 
-  if (target !== data.target) return;
-
   return (
     <div className="dashboard-wrapper">
       <p className="dash__title">
         Сайт: <span className="dash__site-name">{data.target}</span>
       </p>
-      <div className="dash__data">
-        {data.previousData.length > 0 ? (
-          <>
-            <p className="dash__data-string">
-              <BsDatabaseFillCheck size={16} color="var(--primary-color)" />
-              <span>{toShorterDate(data.previousData[0].reportCreatedAt)}</span>
-              <span>
-                завантажено строк в каталозі{' '}
-                <span>{data.previousData[0].dataLength}</span>
-              </span>
-            </p>
-          </>
-        ) : (
-          <p className="dash__data-string">
-            Стягуніть дані. Наступного разу їх можна буде порівняти з новими
-          </p>
-        )}
-        {data.previousData.length > 0 ? (
-          <p className="dash__data-string">
-            Завантажте нові дані для порівняння
-          </p>
-        ) : null}
-      </div>
+      <DashInfoLine
+        props={{ statusLine: 'Test status line', dataLength: 100 }}
+      />
       <div>
         <ProgressButton
           onClick={handleGetData}
